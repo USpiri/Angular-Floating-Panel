e9f0fc120a9a14d2d6b9a90429c07c3afa996f22
unknown <urielspiridione135@gmail.com>
2022-10-15 23:54:12 -0300

    deleted all

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,22 +2,14 @@ import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { MultipleDraggableModalsModule } from 'multiple-draggable-modals';
-import { ModalContainerComponent } from './components/app-root/modal-container/modal-container.component';
-import { AppRootComponent } from './components/app-root/app-root.component';
-import { PanelComponent } from './components/app-root/modal-container/panel/panel.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ModalContainerComponent,
-    AppRootComponent,
-    PanelComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    MultipleDraggableModalsModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent],
